e85b49f07f02436889e41b9337bfadb8c6bb0de7
om-tripathi-1 <om.tripathi@axeno.co>
2026-08-17 12:07:22 +0530

    wrote code

--- script.js
+++ script.js
@@ -55,6 +55,13 @@ function countConsonants(str) {
 // 10. Get First and Last Character
 function getFirstAndLastChar(str) {
     // TODO: Write code to return first and last character of the string
+    let str = "Hello World";
+
+    let firstChar = str[0];
+    let lastChar = str[str.length - 1];
+
+    console.log("First character:", firstChar);
+    console.log("Last character:", lastChar);
     return '';
 }
 
