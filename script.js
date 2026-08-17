6aded2c33d6330c7b239235174a64baa7b116343
Tanish2879 <tanish.mattoo@axeno.co>
2026-08-17 12:32:24 +0530

    Implemented the Remove Spaces function

--- script.js
+++ script.js
@@ -43,8 +43,7 @@ function countVowels(str) {
 
 // 8. Remove All Spaces
 function removeSpaces(str) {
-    // TODO: Write code to remove all spaces from the string
-    return '';
+    return str.replaceAll(' ','');
 }
 
 // 9. Count Consonants
