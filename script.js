7c4c8543dcaf4016f9d5817db54f1637991d89e9
Rohan-axeno <rohan.kumar@axeno.co>
2026-08-17 12:42:31 +0530

    rohan-kumar-count-vowels

--- script.js
+++ script.js
@@ -36,8 +36,26 @@ function capitalizeWords(str) {
 
 // 7. Count Vowels
 function countVowels(str) {
-    // TODO: Write code to count vowels (a, e, i, o, u)
-    return 0;
+    let count = 0;
+
+    for (let i = 0; i < str.length; i++) {
+        if (
+            str[i] === 'a' ||
+            str[i] === 'e' ||
+            str[i] === 'i' ||
+            str[i] === 'o' ||
+            str[i] === 'u' ||
+            str[i] === 'A' ||
+            str[i] === 'E' ||
+            str[i] === 'I' ||
+            str[i] === 'O' ||
+            str[i] === 'U'
+        ) {
+            count++;
+        }
+    }
+
+    return count;
 }
 
 // 8. Remove All Spaces
