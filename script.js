0bed4bffc4f975c12f66849db106a28a19c9c21b
om-tripathi-1 <om.tripathi@axeno.co>
2026-08-17 12:34:16 +0530

    implement the function get-first-and-last-character

--- script.js
+++ script.js
@@ -55,10 +55,13 @@ function countConsonants(str) {
 // 10. Get First and Last Character
 function getFirstAndLastChar(str) {
     // TODO: Write code to return first and last character of the string
-    return '';
+    let firstChar = str[0];
+    let lastChar = str[str.length - 1];
+
+    return [firstChar, lastChar];
 }
 
-// 11. Convert to Lowercase
+// 11. Convert to Lowercase.
 function toLowercase(str) {
     // TODO: Write code to convert the string to lowercase
     return '';
